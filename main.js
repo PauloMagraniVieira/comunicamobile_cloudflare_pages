8cc2d355a863e056c838d3dc2383df0935517d0c
Paulo Magrani Vieira <167261620+PauloMagraniVieira@users.noreply.github.com>
2025-03-15 15:31:32 -0300

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
-// main.js
-document.addEventListener("DOMContentLoaded", function() {
-  // Possíveis funcionalidades dinâmicas podem ser adicionadas aqui
-  console.log("Comunicá Mobile - Página carregada com sucesso");
+document.addEventListener('DOMContentLoaded', () => {
+    const setLanguage = (lang) => {
+        document.querySelectorAll('h1, h2, p').forEach(element => {
+            element.hidden = element.lang !== lang;
+        });
+    };
+    
+    // Animation on scroll
+    const scrollElements = document.querySelectorAll(".scroll-element");
+    
+    const elementInView = (el, dividend = 1) => {
+        const elementTop = el.getBoundingClientRect().top;
+        return (elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend);
+    };
+    
+    const displayScrollElement = (element) => {
+        element.classList.add('scrolled');
+    };
+    
+    const hideScrollElement = (element) => {
+        element.classList.remove('scrolled');
+    };
+    
+    const handleScrollAnimation = () => {
+        scrollElements.forEach(el => {
+            if (elementInView(el, 1.25)) {
+                displayScrollElement(el);
+            } else {
+                hideScrollElement(el);
+            }
+        })
+    }
+    
+    window.addEventListener('scroll', handleScrollAnimation);
 });
